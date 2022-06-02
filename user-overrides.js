51d381b6f1d8a719cce436da198e2b4705186ed3
scogliera <96754695+scogliera@users.noreply.github.com>
2022-06-02 02:29:05 +0200

    Update user-overrides.js

--- user-overrides.js
+++ user-overrides.js
@@ -1,3 +1,14 @@
-
+/* Enable Searching Via The URL Bar ***/
 user_pref("keyword.enabled", true);
+
+/* Disable ALT Keybind ***/
 user_pref("ui.key.menuAccessKeyFocuses", false);
+
+/* Enable Session Restore ***/
+user_pref("browser.startup.page", 3);
+user_pref("browser.sessionstore.privacy_level", 0);
+user_pref("privacy.clearOnShutdown.history", false);
+user_pref("privacy.cpd.history", false);
+
+/* Enable DRM ***/
+user_pref("media.eme.enabled", true);
