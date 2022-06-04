106be1a4477b2fa6aabd5c75e9192d15f08a10e6
scogliera <96754695+scogliera@users.noreply.github.com>
2022-06-04 18:42:41 +0200

    Disabled ALT + Scroll Up/Down binds

--- user-overrides.js
+++ user-overrides.js
@@ -1,9 +1,6 @@
 /* Enable Searching Via The URL Bar ***/
 user_pref("keyword.enabled", true);
 
-/* Disable ALT Keybind ***/
-user_pref("ui.key.menuAccessKeyFocuses", false);
-
 /* Enable Session Restore ***/
 user_pref("browser.startup.page", 3);
 user_pref("browser.sessionstore.privacy_level", 0);
@@ -12,3 +9,11 @@ user_pref("privacy.cpd.history", false);
 
 /* Enable DRM ***/
 user_pref("media.eme.enabled", true);
+
+
+/* Keybinds ***/
+/* Disable ALT Keybind ***/
+user_pref("ui.key.menuAccessKeyFocuses", false);
+
+/* Disable ALT + Scroll Up/Down as Forward/Back page hotkeys***/
+user_pref("mousewheel.with_alt.action", 0);
