1bfb524834ca8d46f28dc8bfcd682a15da80b1e4
Samuele Cannas <96754695+scogliera@users.noreply.github.com>
2025-05-23 15:36:05 +0100

    Update user-overrides.js
    Enable webassembly, enable DRM content, disable data clearing on shutdown

--- user-overrides.js
+++ user-overrides.js
@@ -1,9 +1,27 @@
+/* Test if needed - Enable WASM */
+user_pref("javascript.options.wasm", true);
+
+/* Enable DRM content */
+user_pref("media.eme.enabled", true);
+user_pref("browser.eme.ui.enabled", true);
+
+/* Clear on shutdown fixes */
+user_pref("privacy.clearOnShutdown_v2.historyFormDataAndDownloads", false);
+user_pref("privacy.clearOnShutdown_v2.siteSettings", false);
+user_pref("privacy.clearOnShutdown_v2.browsingHistoryAndDownloads", false);
+user_pref("privacy.clearOnShutdown_v2.downloads", false);
+user_pref("privacy.clearOnShutdown.openWindows", false);
+
+/* Enable Favicon saving */
+user_pref("browser.chrome.site_icons", true);
+
 /* Enable Searching Via The URL Bar ***/
 user_pref("keyword.enabled", true);
 
-/* Enable Session Restore ***/
+/* Enable Session Restore */
 user_pref("browser.startup.page", 3);
 user_pref("browser.sessionstore.privacy_level", 0);
+user_pref("browser.privatebrowsing.autostart", false);
 user_pref("privacy.clearOnShutdown.history", false);
 user_pref("privacy.cpd.history", false);
 
